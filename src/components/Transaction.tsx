b503a81c25f9ba78a14fc79325b67fcc0b8041d2
Mogakamo <mogaka.amo254@gmail.com>
2022-07-13 04:28:10 +0300

    commit

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -1,22 +1,18 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { GET_TRANSACTIONS_QUERY } from "../queries/getTransactions";
 
 const Transaction = () => {
-    const { loading, error, data } = useQuery(GET_TRANSACTIONS_QUERY);
-    const transactions = data?.getTransaction?.getTransactions
-    console.log(transactions?.transaction_id);
     
 
-    if (loading) return <p>Loading ...</p>;
+    // if () return <p>Loading ...</p>;
   
-    if (error) return (
-      <pre>{JSON.stringify(error, null, 2)}</pre>
-    );
+    // if () return (
+    //   // <pre>{JSON.stringify(error, null, 2)}</pre>
+    // );
   
     return (
-      <pre>{JSON.stringify(transactions, null, 2)}</pre>
-    );
+      // <pre>{JSON.stringify(transactions, null, 2)}</pre>
+      <div>data</div>
+      );
 
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./styles/index.css";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { client } from "./utils/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
+    {/* <ApolloProvider client={client}> */}
       <App />
-    </ApolloProvider>
+    {/* </ApolloProvider> */}
   </React.StrictMode>
 );
