197db77b58e2d201e8f6b66d6c07f5354a7a9c0a
Mogakamo <mogaka.amo254@gmail.com>
2022-07-13 15:03:23 +0300

    done with the ui - remaining with the filter buttons

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -45,38 +45,59 @@ const Characters = () => {
   console.log(data);
 
   return (
-    <div>
+    <div className="space-y-10">
       <input
         type="text"
         placeholder="Search..."
         onChange={(e) => setSearchTerm(e.target.value)}
         className="h-12 rounded-lg border w-96 focus:border-gray-400 p-3 outline-none"
       />
-      {data.locations.results.map((location: any) => {
-        return (
-          <>
-            <h2 className="text-4xl font-bold">{location.name}</h2>
-            <div key={location.name} className="grid grid-cols-5 gap-3">
-              {location.residents.map((resident: any) => {
-                if (
-                  resident.name.toLowerCase().includes(searchTerm.toLowerCase())
-                ) {
+
+      <div className="space-x-10">
+        <button className="">All</button>
+        <button className="">male</button>
+        <button className="">female</button>
+      </div>
+
+      {data.locations.results
+        .filter((location: any) => {
+          if (searchTerm === "") {
+            return location;
+          } else if (
+            location.name.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return location;
+          }
+        })
+        .map((location: any) => {
+          return (
+            <div key={location.name}>
+              <h2 className="text-4xl font-bold pb-4">{location.name}</h2>
+              <div key={location.name} className="grid grid-cols-5 gap-3">
+                {location.residents.map((resident: any) => {
                   return (
                     <div className="">
-                      <div key={resident.id} className="flex items-start flex-col">
-                        <h3>{resident.name}</h3>
+                      <div
+                        key={resident.id}
+                        className="flex items-start flex-col"
+                      >
                         <p>
                           <img src={resident.image} alt={resident.name} />
                         </p>
+                        <div className="space-y-2">
+                          <h3>{resident.name}</h3>
+                          <h1>{resident.gender}</h1>
+                        </div>
+                        <h1>{resident.status}</h1>
+                        <h1>{resident.episode.air_date}</h1>
                       </div>
                     </div>
                   );
-                }
-              })}
+                })}
+              </div>
             </div>
-          </>
-        );
-      })}
+          );
+        })}
     </div>
   );
 };
